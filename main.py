73862ff3f4d154cce534cbbf36aebdda4ce030cf
NorthernChicken <144752748+NorthernChicken@users.noreply.github.com>
2023-12-13 11:44:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 """
 This is the main loop file for our AutoTube Bot!
 
-Quick notes!
-- Currently it's set to try and post a video then sleep for a day.
-- You can change the size of the video currently it's set to post shorts.
-    * Do this by adding a parameter of scale to the image_save function.
-    * scale=(width,height)
+This script has been modified.
 """
 
 from datetime import date
 import time
 from utils.CreateMovie import CreateMovie, GetDaySuffix
 from utils.RedditBot import RedditBot
-from utils.upload_video import upload_video
+
+#Subreddit the bot pulls images from (try subs with lots of images, not videos or text)
+sub = "chatgpt"
 
 #Create Reddit Data Bot
 redditbot = RedditBot()
 
-# Leave if you want to run it 24/7
-while True:
+def create_video():
 
     # Gets our new posts pass if image related subs. Default is memes
-    posts = redditbot.get_posts("memes")
+    posts = redditbot.get_posts(sub)
 
     # Create folder if it doesn't exist
     redditbot.create_data_folder()
@@ -38,20 +35,4 @@ while True:
     # Create the movie itself!
     CreateMovie.CreateMP4(redditbot.post_data)
 
-    # Video info for YouTube.
-    # This example uses the first post title.
-    video_data = {
-            "file": "video.mp4",
-            "title": f"{redditbot.post_data[0]['title']} - Dankest memes and comments {dt_string}!",
-            "description": "#shorts\nGiving you the hottest memes of the day with funny comments!",
-            "keywords":"meme,reddit,Dankestmemes",
-            "privacyStatus":"public"
-    }
-
-    print(video_data["title"])
-    print("Posting Video in 5 minutes...")
-    time.sleep(60 * 5)
-    upload_video(video_data)
-
-    # Sleep until ready to post another video!
-    time.sleep(60 * 60 * 24 - 1)
+create_video()
